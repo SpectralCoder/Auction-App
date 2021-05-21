0a3c80db301576fb3d9683d935ce0014c1da82f4
Mujahidul Islam <mislam171020@bscse.uiu.ac.bd>
2021-05-21 08:45:49 -0800

    heroku live

--- auction/settings.py
+++ auction/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = '/static/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
